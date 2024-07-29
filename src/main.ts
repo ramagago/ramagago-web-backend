cd50cc8c9bfb4e18144ff371853fd604ede5ba78
Rama <rama@Ramas-MacBook-Pro.local>
2024-07-29 16:34:41 -0300

    ssl cert

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const keyFile = fs.readFileSync(process.env.key);
+  const certFile = fs.readFileSync(process.env.cert);
+
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions: {
+      key: keyFile,
+      cert: certFile,
+    },
+  });
 
   const corsOptions: CorsOptions = {
     origin: 'http://localhost:3000',
@@ -12,6 +21,6 @@ async function bootstrap() {
   };
 
   app.enableCors(corsOptions);
-  await app.listen(3001);
+  await app.listen(443);
 }
 bootstrap();
